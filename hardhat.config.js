29fced8d8a28318aab90fe3b2e76e7b9e31e0e0c
2namkyung <iddbxk@daum.net>
2022-06-17 19:15:27 +0900

    add baobab network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const KAIKAS_PRIVATE_KEY = process.env.KAIKAS_PRIVATE_KEY;
 
 module.exports = {
   defaultNetwork: "rinkeby",
   solidity: "0.8.6",
   // solidity: "0.5.6",
   networks: {
+    hardhat: {},
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/aa667dc2bc3246ec87b1a333def3be21`,
       accounts: [PRIVATE_KEY],
@@ -15,6 +17,13 @@ module.exports = {
     matic: {
       url: "https://rpc-mumbai.matic.today",
       accounts: [PRIVATE_KEY],
+    },
+
+    baobab: {
+      url: "https://api.baobab.klaytn.net:8651",
+      accounts: [KAIKAS_PRIVATE_KEY],
+      chainId: 1001,
+      gasPrice: 250000000000,
     },
   },
 
